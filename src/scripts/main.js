c9280cfd74005ecd7a56a4f3d83edb0ae85ff037
Denis Pitsul <denia.pitsul@gmail.com>
2025-05-07 18:00:33 +0300

    add promise basic DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write your code here
+const logoElement = document.querySelector('.logo');
+
+const promise1 = new Promise((resolve, reject) => {
+  resolve();
+});
+
+logoElement.addEventListener('click', () => {
+  promise1.then(successHandler).catch(errorHandler);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 3000);
+});
+
+promise2.then(successHandler).catch(errorHandler);
+
+function successHandler() {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.textContent = 'Promise was resolved!';
+
+  document.body.append(message);
+}
+
+function errorHandler() {
+  const message = document.createElement('div');
+
+  message.classList.add('message');
+  message.classList.add('error-message');
+  message.textContent = 'Promise was rejected!';
+
+  document.body.append(message);
+}
